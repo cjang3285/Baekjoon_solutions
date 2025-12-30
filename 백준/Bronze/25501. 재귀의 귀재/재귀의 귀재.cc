b4e8d3d865204c540d6aa6954264db52421dca4d
JANG CHANWOOK <cjang6199@gmail.com>
2025-12-30 20:39:32 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,32 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
-// 회문 검사
-string recursion(const string& s, int l, int r, int& cnt){
-    cnt++;
-    if(l >= r) { // 좌측 문자와 우측 문자가 만나거나 지나치면 회문 검사 통과 
-        return "1 " + to_string(cnt) + '\n';
-    }
-    else if(s[l] != s[r]) { // 좌측 우측 문자가 다르면 회문 아님
-        return "0 " + to_string(cnt) + '\n';
-    }
-    else { // 좌우측 문자가 아직 안 만났고 같으면 좌우 모두 한칸씩 안쪽으로 가며 일치하는지 검사.
-        return recursion(s, l+1, r-1, cnt);
-    } // else 블록 안에 cnt를 넣으면 안되는 이유 : 처음부터 if, else if로 들어가면 호출이 되었어도 cnt가 증가되지 않음.
+// 정석 풀이 저장 //
+// 1. 재귀 함수: 오직 '판별' 결과만 int로 반환합니다.
+int recursion(const string& s, int l, int r, int& cnt) {
+    cnt++; // 호출 횟수 기록
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1, cnt);
 }
 
-string isPalindrome(const string& s, int& cnt){
-    // s.length()는 부호 없는 정수(size_t)를 반환합니다. 만약 빈 문자열("")이 입력되면 0 - 1이 되어 아주 큰 양수(언더플로우)가 발생할 수 있습니다.
-    // 안전하게 하려면 (int)s.length() - 1로 형변환
-    return recursion(s, 0, (int)s.length()-1, cnt);
+// 2. 인터페이스 함수: 외부에서 호출하기 편하게 감싸줍니다.
+int isPalindrome(const string& s, int& cnt) {
+    return recursion(s, 0, (int)s.length() - 1, cnt);
 }
 
-int main(){
-    int t; 
+int main() {
+    // 입출력 가속
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
     cin >> t;
-    while(t--){
-        int cnt = 0;
+    while (t--) {
         string str;
         cin >> str;
-        cout << isPalindrome(str, cnt);
+        int cnt = 0;
+        
+        // 3. main에서 결과값과 cnt를 분리해서 출력합니다.
+        int result = isPalindrome(str, cnt);
+        cout << result << " " << cnt << "\n";
     }
+    return 0;
 }
