3513a2d3b0d4f1fa71247fe2bb6db7f9c143d954
JANG CHANWOOK <cjang6199@gmail.com>
2026-01-19 14:37:39 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int> pq;   // 기본이 최대 힙
+
+    while(n--) {
+        int x;
+        cin >> x;
+
+        if(x == 0) {
+            if(pq.empty()) {
+                cout << 0 << "\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } 
+        else {
+            pq.push(x);
+        }
+    }
+}
