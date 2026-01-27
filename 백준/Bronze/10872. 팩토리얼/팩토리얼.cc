fcef2e5ebb9bf983e413740842d2436f62ce1359
JANG CHANWOOK <cjang6199@gmail.com>
2026-01-27 10:15:04 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    long long result = 1;  
+    for(int i = 1; i <= n; i++){
+        result *= i;
+    }
+    cout << result;
+}
