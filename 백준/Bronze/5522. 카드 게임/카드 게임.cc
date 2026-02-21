f9874e74c44f6da65a36a548cacd14cc3f869b4c
JANG CHANWOOK <cjang6199@gmail.com>
2026-02-21 23:25:48 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    for(int i =0; i < 5; i++){
+        int j =0;
+        cin >> j;
+        sum += j;
+    }
+    cout << sum;
+}
